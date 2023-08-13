05039cb393f3097ae77607681d124f349f812c17
dzc0d3r <m4dcod3r@protonmail.com>
2023-08-13 01:29:32 +0100

    finished assignment :tada:

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Your code here
+
+
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game")
+
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.left = `${(game.clientWidth / 2) - (dodger.clientWidth / 2)}px`
+
+
+
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 9}px`;
+    }
+}
+
+
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 9}px`;
+        if (left + dodger.clientWidth < game.clientWidth) {
+
+        }
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+
+});
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+moveDodgerLeft()
+
+
